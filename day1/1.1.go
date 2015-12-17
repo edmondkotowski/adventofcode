03e871a282b49d9830eed1b5e2cead07b3587af7
Edmond Kotowski <ekotowski@gmail.com>
2015-12-17 13:57:55 -0800

    Fix spacing

--- day1/1.1.go
+++ day1/1.1.go
@@ -6,24 +6,24 @@ import (
 )
 
 func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+    if e != nil {
+        panic(e)
+    }
 }
 
 func main() {
-	data, err := ioutil.ReadFile("input.txt")
-	check(err)
+    data, err := ioutil.ReadFile("input.txt")
+    check(err)
     
     var floor int = 0
 
-	for i := 0; i < len(data); i++ {
-		if data[i] == '(' {
-			floor++
-		} else if data[i] == ')' {
-			floor--
-		}
-	}
+    for i := 0; i < len(data); i++ {
+        if data[i] == '(' {
+		    floor++
+        } else if data[i] == ')' {
+            floor--
+        }
+    }
 
-	fmt.Printf("%d\n", floor)
+    fmt.Printf("%d\n", floor)
 }
