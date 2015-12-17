725e72d91054dda3cfc8a9a9e41e6fa6ffa3d5fe
Edmond Kotowski <ekotowski@gmail.com>
2015-12-17 13:52:57 -0800

    Solution to day 1 part 1

---
+++ day1/1.1.go
@@ -0,0 +1,29 @@
+package main
+	
+import (
+    "fmt"
+    "io/ioutil"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	data, err := ioutil.ReadFile("input.txt")
+	check(err)
+    
+    var floor int = 0
+
+	for i := 0; i < len(data); i++ {
+		if data[i] == '(' {
+			floor++
+		} else if data[i] == ')' {
+			floor--
+		}
+	}
+
+	fmt.Printf("%d\n", floor)
+}
